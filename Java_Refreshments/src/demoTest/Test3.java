d582f9cfd637b165b5f4e8ad0af03aafa0b9d715
Bhadravathi Srinivasa Murthy <nmurthy@galaxe.com>
2024-05-24 10:45:33 +0530

    Adding java Collections

---
+++ Java_Refreshments/src/demoTest/Test3.java
@@ -0,0 +1,30 @@
+package demoTest;
+
+public class Test3 {
+
+	// Switch Case Statements
+	public static void main(String[] args) {
+		
+	 int value = 100;
+
+	 switch (value) {
+	 
+	 case 300 : System.out.println("Number is 300");
+	 break;
+	 
+	 case 200 : System.out.println("Number is 200");
+	 break;
+	 
+	 case 400 : System.out.println("Number is 400");
+	 break;
+	 
+	 case 100 : System.out.println("Number is 100");
+	 break;
+	 
+	 default : 
+		 System.out.println("Value is Not Number");
+	 
+	 }
+	 	
+	}
+}
