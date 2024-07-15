4c85f5ab2ba5c94ece6f610c83c2681b45d818a4
NagendraBS <bsnagendra612@gmail.com>
2024-07-15 23:31:28 +0530

    new CRUD operation With Spring

---
+++ crud-operations-demo/src/main/java/com/mvn/crud_operations_demo/CrudOperationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvn.crud_operations_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationsDemoApplication.class, args);
+	}
+
+}
