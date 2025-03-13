c4f6a24a90db9a3d46d416ab00e38e6f1b109a9e
nmurthy <Bsy@04102000>
2025-03-13 15:32:55 +0530

    DT VAR Static

---
+++ JavaScratchLearnings/src/com/java/Variables/InstanceVariables.java
@@ -0,0 +1,29 @@
+package java.com.Variables;
+
+public class InstanceVariables {
+
+	
+	public String name;
+	public int i ;
+	public Integer I;
+
+
+	public  InstanceVariables() {
+		this.name = "Nagendra nag";
+	}
+
+
+	public static void main(String[] args) {
+
+        // Object Creation
+		 InstanceVariables iv = new InstanceVariables();
+		 
+		 System.out.println("Name is :" + iv.name);
+		 System.out.println("Default value for int is :" + iv.i);
+		 
+	     // toString() called internally
+		 System.out.println("Name is :" + iv.I);
+		
+	}
+
+}
