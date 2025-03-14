ff7b255a3627c920810e534925f6132c308de6d0
nmurthy <Bsy@04102000>
2025-03-14 11:23:15 +0530

    java Decision Makings

---
+++ JavaScratchLearnings/src/com/java/DecisionMaking/Switch_Case.java
@@ -0,0 +1,37 @@
+package com.java.DecisionMaking;
+
+public class Switch_Case {
+
+	public static void main(String[] args) {
+
+		//Declaring a variable for switch expression  
+		int num = 20;
+		
+	    //Switch expression  
+		switch(num) {
+		
+	    //Case statements  
+		case 5 : 
+			System.out.println("it is 5");
+			break;
+			
+		case 10 :
+			System.out.println("it is 10");
+			break;
+			
+		case 15 : 
+			System.out.println("it is 15");
+			break;
+			
+		case 20 :
+			System.out.println("it is 20");
+			break;
+			
+		//Default case statement  
+		default:
+			System.out.println("Not in 5, 10 or 15");
+			
+		}
+	}
+
+}
