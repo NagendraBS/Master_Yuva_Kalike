5dfb66bab6750f2c8691f638e29babeaab37b334
nmurthy <Bsy@04102000>
2025-07-02 22:18:49 +0530

    Resume

--- JavaPracticesPrograms/src/com/java/streams/EmployeesDiffOperations.java
+++ JavaPracticesPrograms/src/com/java/streams/EmployeesDiffOperations.java
@@ -99,11 +99,10 @@ public class EmployeesDiffOperations {
 		Map<String, Integer> totalSalarybyDept = employees.stream().
 				collect(Collectors.groupingBy(Emp::getDepartment, Collectors.summingInt(Emp::getSalary)));
 		
-		
-		
-		return totalSalarybyDept;
+		return totalSalarybyDept ;
 		
 	}
+	
 	
 	
 
