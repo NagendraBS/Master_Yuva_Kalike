337a5114ecfd36160318f3456fcbd4ee895b5013
nmurthy <Bsy@04102000>
2025-07-02 15:35:57 +0530

    Apache Jmeter

--- JavaPracticesPrograms/src/com/java/interview/EqualsMethod.java
+++ JavaPracticesPrograms/src/com/java/interview/EqualsMethod.java
@@ -5,12 +5,15 @@ public class EqualsMethod {
 	public static void main(String[] args) {
 
 		String str1 = "Nagendra";
-		
+		String str3 = "Nagendra";
+
 		String str2 = new String("Nagendra");
 		
 		System.out.println(str1==str2);
+		
 		System.out.println(str1.equals(str2));
 		
+		System.out.println(str1==str3);
 		
 	}
 
